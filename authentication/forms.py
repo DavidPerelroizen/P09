d6678079689ab10ebcbea1fd19ce43fb6609344a
David Perelroizen <david.perelroizen@gmail.com>
2022-03-07 14:38:49 +0100

    Added the username field in the mandatories forms

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,8 @@
-from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=20, label='username')
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput, label='password')
+class RegisterForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ['username']
