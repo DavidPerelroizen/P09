c9870065d60c628293f3fe7b1ac9ae77fb5145d3
David Perelroizen <david.perelroizen@gmail.com>
2022-03-09 15:45:36 +0100

    Updated the home_page view in order to collect all the existing tickets

--- feedapp/views.py
+++ feedapp/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from . import forms
+from . import forms, models
 
 # Create your views here.
 
 
 @login_required
 def home_page(request):
-    return render(request, 'feedapp/home_page.html')
+    tickets = models.Ticket.objects.all()
+    return render(request, 'feedapp/home_page.html', context={'tickets': tickets})
 
 
 @login_required
 def ticket_creation(request):
     ticket_form = forms.TicketForm()
     if request.method == 'POST':
-        ticket_form = forms.TicketForm(request.POST)
+        ticket_form = forms.TicketForm(request.POST, request.FILES)
         if ticket_form.is_valid():
             ticket_form.user = request.user
-            ticket_form.save(commit=False)
+            ticket_form.save()
             return redirect('home_page')
     return render(request, 'feedapp/create_ticket.html', context={'ticket_form': ticket_form})
