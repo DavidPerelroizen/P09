0682c819bab1ff6a1d65b8f66323cd9cd84bb211
David Perelroizen <david.perelroizen@gmail.com>
2022-03-09 19:00:36 +0100

    Added a view for review creation

--- feedapp/views.py
+++ feedapp/views.py
@@ -17,7 +17,21 @@ def ticket_creation(request):
     if request.method == 'POST':
         ticket_form = forms.TicketForm(request.POST, request.FILES)
         if ticket_form.is_valid():
+            ticket_form.save(commit=False)
             ticket_form.user = request.user
             ticket_form.save()
             return redirect('home_page')
     return render(request, 'feedapp/create_ticket.html', context={'ticket_form': ticket_form})
+
+
+@login_required
+def review_creation(request):
+    review_form = forms.ReviewForm()
+    if request.method == 'POST':
+        review_form = forms.ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save(commit=False)
+            review_form.user = request.user
+            review_form.save()
+            return redirect('home_page')
+    return render(request, 'feedapp/create_review.html', context={'review_form': review_form})
