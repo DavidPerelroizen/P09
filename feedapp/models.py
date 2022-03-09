bcd2747c3dd404f64e75b0655db07cbde055f9dd
David Perelroizen <david.perelroizen@gmail.com>
2022-03-09 10:28:43 +0100

    Created the feedapp models

--- feedapp/models.py
+++ feedapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from LITReview import settings
 
 # Create your models here.
+
+
+class Ticket:
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class Review:
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(max_length=1024, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class UserFollows:
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
+
+    class Meta:
+        unique_together = ('user', 'followed_user',)
+
+
