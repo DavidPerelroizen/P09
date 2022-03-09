fe820e74ae90b4a46096f682a4bd37fe97f1c337
David Perelroizen <david.perelroizen@gmail.com>
2022-03-09 11:42:54 +0100

    Created the ticket creation view

--- feedapp/views.py
+++ feedapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
@@ -7,3 +8,15 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home_page(request):
     return render(request, 'feedapp/home_page.html')
+
+
+@login_required
+def ticket_creation(request):
+    ticket_form = forms.TicketForm()
+    if request.method == 'POST':
+        ticket_form = forms.TicketForm(request.POST)
+        if ticket_form.is_valid():
+            ticket_form.user = request.user
+            ticket_form.save(commit=False)
+            return redirect('home_page')
+    return render(request, 'feedapp/create_ticket.html', context={'ticket_form': ticket_form})
