378052169a7498b8c821024bca2a1599efea1817
David Perelroizen <david.perelroizen@gmail.com>
2022-03-09 16:03:52 +0100

    Added a resize image function and surcharged it to the save function so that images size would be limited

--- feedapp/models.py
+++ feedapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from LITReview import settings
+from PIL import Image
 
 # Create your models here.
 
@@ -11,6 +12,18 @@ class Ticket(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
+
+    IMAGE_MAX_SIZE = (800, 800)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
+
 
 
 class Review(models.Model):
