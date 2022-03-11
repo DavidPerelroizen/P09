667948fb6031b3594a14ec3e3883beb3cd542e0d
David Perelroizen <david.perelroizen@gmail.com>
2022-03-11 12:10:53 +0100

    Corrected review_creation and ticket_creation in order to avoid user_id error

--- feedapp/views.py
+++ feedapp/views.py
@@ -13,24 +13,25 @@ def home_page(request):
 
 @login_required
 def ticket_creation(request):
-    user_id = request.user
-    ticket_form = forms.TicketForm(user_id=user_id)
+    form = forms.TicketForm()
     if request.method == 'POST':
-        ticket_form = forms.TicketForm(request.POST, request.FILES)
-        if ticket_form.is_valid():
+        form = forms.TicketForm(request.POST, request.FILES)
+        if form.is_valid():
+            ticket_form = form.save(commit=False)
             ticket_form.user = request.user
             ticket_form.save()
             return redirect('home_page')
-    return render(request, 'feedapp/create_ticket.html', context={'ticket_form': ticket_form})
+    return render(request, 'feedapp/create_ticket.html', context={'ticket_form': form})
 
 
 @login_required
 def review_creation(request):
-    review_form = forms.ReviewForm()
+    form = forms.ReviewForm()
     if request.method == 'POST':
-        review_form = forms.ReviewForm(request.POST)
-        if review_form.is_valid():
+        form = forms.ReviewForm(request.POST)
+        if form.is_valid():
+            review_form = form.save(commit=False)
             review_form.user = request.user
             review_form.save()
             return redirect('home_page')
-    return render(request, 'feedapp/create_review.html', context={'review_form': review_form})
+    return render(request, 'feedapp/create_review.html', context={'review_form': form})
