2e3abcf7c22fff3e7fdc3a87f89720ca175932a1
David Perelroizen <david.perelroizen@gmail.com>
2022-03-16 19:14:07 +0100

    Added an attribute follows to the User model

--- authentication/models.py
+++ authentication/models.py
@@ -8,3 +8,5 @@ class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
 
     USERNAME_FIELD = 'username'
+
+    follows = models.ManyToManyField('self', symmetrical=False)
