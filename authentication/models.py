0b16d074afe11cb106e9dfcd7bce8152504a2d14
David Perelroizen <david.perelroizen@gmail.com>
2022-03-18 10:38:20 +0100

    User class simplification

--- authentication/models.py
+++ authentication/models.py
@@ -8,5 +8,3 @@ class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
 
     USERNAME_FIELD = 'username'
-
-    follows = models.ManyToManyField('self', symmetrical=False, verbose_name='follows')
